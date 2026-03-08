3a432cf75c6a7cd2de7b63d76838cb4f48792f2f
BETO <test@Betos-MacBook-Air.local>
2026-03-08 15:10:52 -0700

    debug: region response

--- noma/api/hotels.js
+++ noma/api/hotels.js
@@ -12,18 +12,7 @@ export default async function handler(req, res) {
       }}
     );
     const regionData = await regionRes.json();
-    const regionId = regionData?.data?.[0]?.gaiaId;
-    if (!regionId) return res.status(200).json({ data: { propertySearch: { properties: [] } } });
-
-    const hotelsRes = await fetch(
-      'https://hotels-com-provider.p.rapidapi.com/v2/hotels/search?region_id=' + regionId + '&locale=en_US&checkin_date=' + checkIn + '&checkout_date=' + checkOut + '&adults_number=' + (adults||2) + '&sort_order=REVIEW&domain=US&available_filter=SHOW_AVAILABLE_ONLY',
-      { headers: {
-        'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
-        'X-RapidAPI-Host': 'hotels-com-provider.p.rapidapi.com'
-      }}
-    );
-    const data = await hotelsRes.json();
-    res.status(200).json(data);
+    return res.status(200).json({ debug_region: regionData });
   } catch(e) {
     res.status(500).json({ error: e.message });
   }
