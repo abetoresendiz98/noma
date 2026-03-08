7259d4abfd81410a2d177c8b3a15a3d206f470a5
BETO <test@Betos-MacBook-Air.local>
2026-03-08 16:23:22 -0700

    feat: PWA - instalable como app

--- noma/src/main.jsx
+++ noma/src/main.jsx
@@ -2,6 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 
+// Register Service Worker for PWA
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then((reg) => console.log('SW registered:', reg.scope))
+    .catch((err) => console.log('SW error:', err));
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
